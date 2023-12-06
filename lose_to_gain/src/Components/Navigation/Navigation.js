e287828fadf73987fa0780a3226c7fefee47236d
Mohaiminul Islam <mmislam272@gmail.com>
2023-12-06 23:57:31 +0600

    Navigation ongoing

--- lose_to_gain/src/Components/Navigation/Navigation.js
+++ lose_to_gain/src/Components/Navigation/Navigation.js
@@ -1,7 +1,7 @@
 export default function Navigation() {
     return (
         <div className="shadow sticky z-50 top-0">
-            <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
+            <nav className="bg-gray-500 border-gray-200 px-4 lg:px-6 py-2.5">
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                     <Link to="/" className="flex items-center">
                         <img
