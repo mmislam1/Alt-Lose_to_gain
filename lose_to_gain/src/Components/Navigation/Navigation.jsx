a55c28c1519f1bcfad9974bf2a48b42882d7c60b
Mohaiminul Islam <mmislam272@gmail.com>
2023-12-08 23:11:28 +0600

    router ongoing

--- lose_to_gain/src/Components/Navigation/Navigation.jsx
+++ lose_to_gain/src/Components/Navigation/Navigation.jsx
@@ -3,7 +3,7 @@ export default function Navigation() {
         <div className="shadow sticky z-50 top-0">
             <nav className="bg-gray-500 border-gray-200 px-4 lg:px-6 py-2.5">
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-                    <Link to="/" className="flex items-center">
+                    <Link to="#" className="flex items-center">
                         <img
                             src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
                             className="mr-3 h-12"
--- lose_to_gain/src/main.jsx
+++ lose_to_gain/src/main.jsx
@@ -2,9 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import About from './Components/About/About.jsx'
+import Foods from './Components/Foods/Foods.jsx'
+import Chart from './Components/Chart/Chart.jsx'
+
+//<Route path='user/:userid' element={<User />} />
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<About />} />
+      <Route path='Foods' element={<Foods />} />
+      <Route path='Chart' element={<Chart />} />
+      
+      <Route 
+      loader={githubInfoLoader}
+      path='github' 
+      element={<Github />}
+       />
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
