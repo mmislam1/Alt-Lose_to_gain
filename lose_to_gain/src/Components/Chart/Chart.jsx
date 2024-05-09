cb2498a8c2cd6d1f61afa538a3a99f055a2d1ee3
Mohaiminul Islam <mmislam272@gmail.com>
2024-05-09 23:22:18 +0600

    chart

--- lose_to_gain/src/Components/Chart/Chart.jsx
+++ lose_to_gain/src/Components/Chart/Chart.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Foods from '../Foods/Foods.jsx'
 
 const Chart = () => {
+  
   return (
-    <div>Chart</div>
+    <div>
+      <h2>Diet Chart</h2>
+      <div>
+        <ul>
+          <li>
+            <div>
+              <Foods/>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
