5b2d210064303bada58ff4d4447b9e80edc38410
Mohaiminul Islam <mmislam272@gmail.com>
2024-05-12 22:11:08 +0600

    general item list

--- lose_to_gain/src/Components/Foods/List_Item/General_list_Item/GeneralListItem.jsx
+++ lose_to_gain/src/Components/Foods/List_Item/General_list_Item/GeneralListItem.jsx
@@ -3,8 +3,18 @@ import React from 'react'
 const GeneralListItem = () => {
   return (
     <div>
-      <div><h3>{ }</h3></div>
-      <div></div>
+      <div><h5>{ }</h5></div>
+      <div>
+        <div>
+          <h6></h6>
+        </div>
+        <div>
+          <h6></h6>
+        </div>
+        <div>
+          <h6></h6>
+        </div>
+      </div>
       <div></div>
     </div>
   )
