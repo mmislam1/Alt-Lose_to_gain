1922998202bf49cd5986d4500871b7107dcf7eb2
Mohaiminul Islam <mmislam272@gmail.com>
2024-05-12 22:13:00 +0600

    Selected List Item

--- lose_to_gain/src/Components/Foods/List_Item/Selected_list_item/SelectedListItem.jsx
+++ lose_to_gain/src/Components/Foods/List_Item/Selected_list_item/SelectedListItem.jsx
@@ -4,7 +4,20 @@ const SelectedListItem = () => {
   return (
     <div>
       <div><h3>{ }</h3></div>
-      <div></div>
+      <div>
+        <div>
+          <h6></h6>
+          <h6></h6>
+        </div>
+        <div>
+          <h6></h6>
+          <h6></h6>
+        </div>
+        <div>
+          <h6></h6>
+          <h6></h6>
+        </div>
+      </div>
       <div></div>
     </div>
   )
