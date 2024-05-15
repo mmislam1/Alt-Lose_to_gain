abba72761953b07903af0d75336412bee7b9169f
Mohaiminul Islam <mmislam272@gmail.com>
2024-05-15 22:49:21 +0600

    List Items

--- lose_to_gain/src/Components/Foods/Foods.jsx
+++ lose_to_gain/src/Components/Foods/Foods.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import SelectedListItem from './List_Item/Selected_list_item/SelectedListItem.jsx'
+import GeneralListItem from './List_Item/General_list_Item/GeneralListItem.jsx'
 const Foods = () => {
   return (
     <div>
