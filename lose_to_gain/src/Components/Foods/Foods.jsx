7cea81393ddbba1dcbfac20ca5e27cde34b2a21d
Mohaiminul Islam <mmislam272@gmail.com>
2024-05-19 01:52:19 +0800

    re

--- lose_to_gain/src/Components/Foods/Foods.jsx
+++ lose_to_gain/src/Components/Foods/Foods.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import SelectedListItem from './List_Item/Selected_list_item/SelectedListItem.jsx'
-import GeneralListItem from './List_Item/General_list_Item/GeneralListItem.jsx'
+import GeneralListItem from "././List_Item/General_list_item/GeneralListItem.jsx"
 
 
 
