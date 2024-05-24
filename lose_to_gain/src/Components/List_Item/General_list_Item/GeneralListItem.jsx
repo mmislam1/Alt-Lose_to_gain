cc6160b38802da20165c4c2ca08ec6129eb480f3
mmislam <mmislam272@gmail.com>
2024-05-24 21:39:10 +0600

    macro

--- lose_to_gain/src/Components/List_Item/General_list_Item/GeneralListItem.jsx
+++ lose_to_gain/src/Components/List_Item/General_list_Item/GeneralListItem.jsx
@@ -6,16 +6,16 @@ const GeneralListItem = () => {
       <div><h5>{item }</h5></div>
       <div>
         <div>
-          <h6></h6>
-          <h6></h6>
+          <h6>{macro}</h6>
+          <h6>{macro_amount}</h6>
         </div>
         <div>
-          <h6></h6>
-          <h6></h6>
+          <h6>{macro}</h6>
+          <h6>{macro_amount}</h6>
         </div>
         <div>
-          <h6></h6>
-          <h6></h6>
+          <h6>{macro}</h6>
+          <h6>{macro_amount}</h6>
         </div>
       </div>
       <div>
