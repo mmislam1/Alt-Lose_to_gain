b9670e308e4f91e79cdf37248b6ac1c325d09018
mmislam <mmislam272@gmail.com>
2024-05-29 23:09:29 +0600

    structuring

--- lose_to_gain/src/Components/List_Item/Selected_list_item/SelectedListItem.jsx
+++ lose_to_gain/src/Components/List_Item/Selected_list_item/SelectedListItem.jsx
@@ -4,16 +4,16 @@ const SelectedListItem = () => {
   return (
     <div key='' className='flex'>
       <div><h3>{item}</h3></div>
-      <div className='flex'>
-        <div>
+      <div className='flex-row'>
+        <div className='flex-col'>
           <h6>{macro}</h6>
           <h6>{macro_amount}</h6>
         </div>
-        <div>
+        <div className='flex-col'>
           <h6>{macro}</h6>
           <h6>{macro_amount}</h6>
         </div>
-        <div>
+        <div className='flex-col'>
           <h6>{macro}</h6>
           <h6>{macro_amount}</h6>
         </div>
