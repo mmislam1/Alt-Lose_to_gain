b3ba0bb8b71d7af8f9cb7a9d3b381996f7b169f0
mmislam <mmislam272@gmail.com>
2025-02-11 23:05:28 +0600

    foodlist

--- frontend3/src/Components/FoodList/FoodList.js
+++ frontend3/src/Components/FoodList/FoodList.js
@@ -1,22 +1,39 @@
 import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import GeneralListItem from '../List_Item/General_list_item/GeneralListItem'
+import SelectedListItem from '../List_Item/Selected_list_item/SelectedListItem'
 
 const FoodList = () => {
 
   const foodItems = useSelector(state => state.foodItems)
 
-
+  let flag=0
 
 
   return (
-    <div className='foodListContainer'>
-      <div className='foodList'>
-        {
-          foodItems.map((item)=><div></div>)
+    
+      <div className='flex flex-row border-1 border-green-500 rounded-md'>
+      {
+        
+        foodItems.map((item) => {
+          if (flag === 0) {
+            flag = 1
+          }
+          else {
+            return <div class="w-full h-[1px] bg-gray-400"></div>
+          }
+          if (item.selected) {
+            return <SelectedListItem item={item} />
+          }
+          else {
+            return <GeneralListItem item={item} />
+          }
+          
+        })
         }
         
     </div>
-</div>
+
   )
 }
 
